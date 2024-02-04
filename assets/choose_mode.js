914aa54133d4799af25f10f5bfa2b0d74e3680f3
Delyan Angelov <delian66@gmail.com>
2024-02-04 13:28:06 +0200

    Add dark mode (thanks to @MCausc78), (edit: I had to squash the changes from https://github.com/vlang/edu-platform/pull/2 manually here)

---
+++ assets/choose_mode.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const Mode = {DARK: 0, LIGHT: 1};
+
+function defaultSettings() {
+  return {mode: Mode.DARK};
+}
+
+function settings(f = null) {
+  if (f) {
+    const j = f({
+      ...defaultSettings(),
+      ...(JSON.parse(localStorage.getItem('settings') || '{}')),
+    });
+    return j ? (localStorage.setItem('settings', JSON.stringify(j)), j) : j;
+  }
+  return {
+    ...defaultSettings(),
+    ...JSON.parse(localStorage.getItem('settings') || '{}'),
+  };
+}
+
+function switchMode() {
+  const { mode } = settings(j => ({
+    ...j,
+    mode: [Mode.LIGHT, Mode.DARK][j.mode],
+  }));
+  setupMode(mode);
+}
+
+function setupMode(mode) {
+  for (const e of document.getElementsByClassName('dark-theme')) {
+    e.disabled = mode === Mode.LIGHT;
+  }
+}
+
+setupMode(settings().mode);
