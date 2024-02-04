36f615f9f942bff72bdd72f8493d664233aee4a7
Delyan Angelov <delian66@gmail.com>
2024-02-04 14:09:12 +0200

    change the label of the dark/light button, when it is clicked too

--- assets/choose_mode.js
+++ assets/choose_mode.js
@@ -3,35 +3,44 @@
 const Mode = {DARK: 0, LIGHT: 1};
 
 function defaultSettings() {
-  return {mode: Mode.DARK};
+	return {mode: Mode.DARK};
 }
 
 function settings(f = null) {
-  if (f) {
-    const j = f({
-      ...defaultSettings(),
-      ...(JSON.parse(localStorage.getItem('settings') || '{}')),
-    });
-    return j ? (localStorage.setItem('settings', JSON.stringify(j)), j) : j;
-  }
-  return {
-    ...defaultSettings(),
-    ...JSON.parse(localStorage.getItem('settings') || '{}'),
-  };
+	if (f) {
+		const j = f({
+			...defaultSettings(),
+			...(JSON.parse(localStorage.getItem('settings') || '{}')),
+		});
+		return j ? (localStorage.setItem('settings', JSON.stringify(j)), j) : j;
+	}
+	return {
+		...defaultSettings(),
+		...JSON.parse(localStorage.getItem('settings') || '{}'),
+	};
 }
 
 function switchMode() {
-  const { mode } = settings(j => ({
-    ...j,
-    mode: [Mode.LIGHT, Mode.DARK][j.mode],
-  }));
-  setupMode(mode);
+	const { mode } = settings(j => ({
+		...j,
+		mode: [Mode.LIGHT, Mode.DARK][j.mode],
+	}));
+	setupMode(mode);
 }
 
 function setupMode(mode) {
-  for (const e of document.getElementsByClassName('dark-theme')) {
-    e.disabled = mode === Mode.LIGHT;
-  }
+	for (const e of document.getElementsByClassName('dark-theme')) {
+		e.disabled = mode === Mode.LIGHT;
+	}
+	const html = document.querySelector("html");
+	switch (mode) {
+	case Mode.LIGHT:
+		html.className = 'light_mode';
+		break;
+	case Mode.DARK:
+		html.className = 'dark_mode';
+		break;
+	}
 }
 
 setupMode(settings().mode);
